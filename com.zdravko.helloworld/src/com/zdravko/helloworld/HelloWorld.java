b054b1f13da53d1a8e11ad988fc16115eeb54e6e
zrusinov <zdravko_rusinov@abv.bg>
2017-02-19 16:06:14 +0200

    Remove joda library

--- com.zdravko.helloworld/src/com/zdravko/helloworld/HelloWorld.java
+++ com.zdravko.helloworld/src/com/zdravko/helloworld/HelloWorld.java
@@ -1,12 +1,16 @@
 package com.zdravko.helloworld;
 
-import org.joda.time.LocalTime;
+//import org.joda.time.LocalTime;
+
+import java.util.Date;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		LocalTime currentTime = new LocalTime();
-		System.out.println("The current local time is: " + currentTime);
+		//LocalTime currentTime = new LocalTime();
+
+        //System.out.println("The current local time is: " + currentTime);
+        System.out.println("The current local time is: "  +  new Date());
 		System.out.println("Hello World");
 
 	}
